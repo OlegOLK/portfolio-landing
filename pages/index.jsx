22c35bb1e5420d67d06ba83e487184a6056e3f1e
11011 <ffsigmapp@gmail.com>
2021-07-06 23:03:14 +0200

    next iteration inprogress

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Navbar from './../components/nav/nav';
 import MyNav from '../components/nav/navbar';
 import DisctordPopup from '../components/discord/discord'
 import More from '../components/more/more'
+import { ArrowRightIcon, UserIcon, FingerPrintIcon } from '@heroicons/react/outline'
 
 export default function Home() {
   return (
@@ -51,42 +52,156 @@ export default function Home() {
 
         </div>
       </div>
-<div className="h-screen">
-    <video controls className="w-full h-full mt-16">
-  <source src="movie.mp4" type="video/mp4" />
-  <source src="movie.ogg" type="video/ogg" />
-  Your browser does not support the video tag.
+      <div className="h-screen z-40 relative">
+        <div className="relative w-full video bg-gray-400 mt-32">
+          <div className="absolute inset-y-1/2 play-button bg-red-500 flex items-center justify-center">
+            <div className="playIcon -ml-1">
+              <svg width="64" cheight="62" viewBox="0 0 64 62" fill="#7DC7A8" xmlns="http://www.w3.org/2000/svg">
+                <path d="M19.1933 16.6405C19.1933 13.1764 22.9433 11.0113 25.9433 12.7434L58.9433 31.7959C61.9433 33.528 61.9433 37.8581 58.9433 39.5902L25.9433 58.6427C22.9433 60.3748 19.1933 58.2097 19.1933 54.7456L19.1933 16.6405Z" fill="#7DC7A8" stroke="#F3F3F3" />
+              </svg>
 
-    </video>
-</div>
+            </div>
+          </div>
+        </div>
+        {/* <video controls className="w-full h-full mt-16">
+          <source src="movie.mp4" type="video/mp4" />
+          <source src="movie.ogg" type="video/ogg" />
+            Your browser does not support the video tag.
+        </video> */}
+      </div>
 
-<div className="h-screen container mx-auto grid w-full grid-cols-2 gap-4 relative">
-  <div>
-  Enjoy automatic updates 
-of your wallets
+      <div className="h-screen container mx-auto grid w-full grid-cols-2 gap-x-4 gap-y-44	 relative z-0 mt-8">
+        <div className="flex justify-items-start space-x-2 mt-20">
+          <div>
+            <img src="cube_1.svg" className="inline" />
+
+          </div>
+          <div>
+
+
+            <div className="feature-header">
+              Enjoy automatic updates <br />
+          of your wallets
   </div>
-<div className="flex justify-end items-start">
+            <div className="feature-subheader mt-4">
+              Track your investments based on the <br />
+  matematically perfect analytics. Get a clear <br />
+   overview of your profits on multiple levels.
+  </div>
+            <div>
+              <button
+                className="relative font-main bg-none border-0 text-left  py-3 block text-base font-semibold main-color ml-0">
+
+                Learn more
+                                    <ArrowRightIcon className="inline h-5 w-5 ml-2" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-end items-start">
           <img
-            className="w-auto -mt-16"
-            src="./test.svg">
+            className="w-auto -mt-12"
+            src="./feature_1.png">
           </img>
 
 
         </div>
-</div>
-      {/* </main> */}
+        
+        <div className="mt-16">
+          <div className="flex justify-start items-start">
+            <img
+              className="w-auto -mt-12"
+              src="./feature_2.png">
+            </img>
 
-      {/* <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer> */}
+
+          </div>
+        </div>
+        <div className="flex items-end">
+        <div className="flex justify-items-start space-x-2">
+          <div>
+            <img src="cube_1.svg" className="inline" />
+
+          </div>
+          <div>
+
+
+            <div className="feature-header">
+            Get a multi currency  <br/>
+support
+  </div>
+            <div className="feature-subheader mt-4">
+            Track your trades performance not only in USD and <br/>
+            BTC! - TradeFo calculates  profits in many <br/>
+            currencies. Choose any 2 base currencies you <br/>
+            want and switch between them on the fly! 
+  </div>
+            <div>
+              <button
+                className="relative font-main bg-none border-0 text-left  py-3 block text-base font-semibold main-color ml-0">
+
+                Learn more
+                                    <ArrowRightIcon className="inline h-5 w-5 ml-2" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        </div>
+
+
+        <div className="flex items-center relative">
+          <div className="absolute -bottom-12 -left-44">
+          <figure className="cube inline-block mr-4 ">
+
+</figure>
+           <figure className="cube inline-block">
+
+           </figure>
+          </div>
+        <div className="flex justify-items-start space-x-2">
+          <div>
+            <img src="cube_1.svg" className="inline" />
+
+          </div>
+          <div>
+
+
+            <div className="feature-header">
+            Set your cost basis
+  </div>
+            <div className="feature-subheader mt-4">
+            Specify the initial cost of each asset in your <br/>
+            Portfolio and enjoy precise overview.
+  </div>
+            <div>
+              <button
+                className="relative font-main bg-none border-0 text-left  py-3 block text-base font-semibold main-color ml-0">
+
+                Learn more
+                                    <ArrowRightIcon className="inline h-5 w-5 ml-2" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        </div>
+
+
+        <div className="mt-16">
+          <div className="flex justify-start items-start">
+            <img
+              className="w-auto -mt-12"
+              src="./feature_3.png">
+            </img>
+
+
+          </div>
+        </div>
+      
+      </div>
+
     </div>
   )
 }
