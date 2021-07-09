3f8104d99018fc4afac7cb953b5b4d56534ae671
11011 <ffsigmapp@gmail.com>
2021-07-09 15:57:01 +0200

    adjustments

--- components/accordion/accordion.tsx
+++ components/accordion/accordion.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent, useState } from 'react';
-import { PlusIcon } from '@heroicons/react/solid'
+import { PlusIcon, MinusIcon } from '@heroicons/react/solid'
 
 export type AccordtionProps = {
     header: string;
@@ -15,7 +15,7 @@ export const Accordion: FunctionComponent<AccordtionProps> = ({ header, subHeade
     }
 
         return (
-            <div className="flex justify-start w-full h-full">
+            <div className="flex justify-start w-full h-full cursor-pointer" onClick={handleClick}>
                 <div className="flex flex-wrap py-5 items-center bg-main-color flex-grow  rounded-md align-middle" >
                     <p className="text-white ml-12 font-bold text-2xl	">{header}</p>
 
@@ -26,7 +26,11 @@ export const Accordion: FunctionComponent<AccordtionProps> = ({ header, subHeade
                 <div className="flex-none">
                     <button className="border-0 bg-transparent flex items-center w-10 h-10 mt-5  "
                         onClick={handleClick}>
-                        <PlusIcon className="inline-block main-color " />
+                        {
+                            toggled ? 
+                            <MinusIcon className="inline-block main-color " /> :
+                            <PlusIcon className="inline-block main-color " />
+                        }
                     </button>
                 </div>
             </div>
--- components/nav/navbar.jsx
+++ components/nav/navbar.jsx
@@ -6,35 +6,35 @@ export default function MyNav() {
             <div className="container mx-auto ">
                 <div className="relative flex items-center  justify-between h-28">
                     <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                        <div className="flex-shrink-0 flex items-center">
+                        <div className="flex-shrink-0 flex items-center cursor-pointer">
                             <img
                                 className="block lg:hidden h-8 w-auto"
                                 src="/main.svg"
-                                alt="Workflow"
+                                alt="logo"
                             />
                             <img
                                 className="hidden lg:block h-8 w-auto"
                                 src="/main.svg"
-                                alt="Workflow"
+                                alt="logo"
                             />
                         </div>
-                        <div className="hidden sm:block sm:ml-6">
+                        <div className="hidden sm:block sm:ml-6 cursor-pointer">
                             <div className="flex space-x-8 text-center">
                                 <div className=" md:mr-24">
                                     <a className="text-black logo">TradeFo</a>
                                 </div>
-                                <a className="secondary-menu self-center">Features</a>
-                                <a className="secondary-menu self-center">Pricing</a>
-                                <a className="secondary-menu self-center">Contact</a>
+                                <a className="secondary-menu cursor-pointer self-center">Features</a>
+                                <a className="secondary-menu cursor-pointer self-center">Pricing</a>
+                                <a className="secondary-menu cursor-pointer self-center">Contact</a>
 
                                 <div className="absolute right-0 flex items-center justify-center">
                                     <button
-                                        className="relative bg-none border-0  w-44 py-3 block text-base font-semibold main-color">
+                                        className="relative hover:opacity-90 hover:scale-105 bg-none border-0  w-44 py-3 block text-base font-semibold main-color">
                                         <FingerPrintIcon className="inline h-5 w-5 mr-2" aria-hidden="true" />
                                     Sign in
                                 </button>
                                     <button
-                                        className="relative shadow-lg w-44 py-3 block text-base font-semibold bg-main-color rounded-lg text-white ">
+                                        className="relative hover:opacity-90 hover:scale-105 shadow-lg w-44 py-3 block text-base font-semibold bg-main-color rounded-lg text-white ">
                                         Get Started
                             </button>
                                 </div>
@@ -43,10 +43,6 @@ export default function MyNav() {
 
                         </div>
                     </div>
-                    {/* <div className="inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                                // py-3 px-4 focus:outline-none focus-visible:ring-2 focus-visible:ring-teal-600 focus-visible:ring-offset-2 hover:bg-teal-500
-                   
-                </div> */}
                 </div>
             </div>
         </nav>
