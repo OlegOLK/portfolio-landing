feb0ec8d35a86c0f9e68166eb8e4e2832e38fe38
Oleg Krotenko <ok@includeagency,com>
2021-09-09 10:48:34 +0300

    clean up

--- pages/_document.js
+++ pages/_document.js
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}} />
+    })(window,document,'https://static.hotjar.com/c/hotjar-${HJ_TRACKING_ID}','.js?sv=');`}} />
 
           {/* Google optimize */}
           <script src={`https://www.googleoptimize.com/optimize.js?id=${OPT_TRACKING_ID}`} />
