24228116865ba2db8dc589737bc62315b86ff426
Oleg Krotenko <ok@includeagency,com>
2021-09-09 10:33:19 +0300

    fix env variables

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 const nextTranslate = require('next-translate')
 
-module.exports = nextTranslate({
-    env: {
-        "NEXT_PUBLIC_HOTJAR_ID": "1",
-        "NEXT_PUBLIC_OPTIMIZE_ID": "1"
-    }
-})
+module.exports = nextTranslate({})
